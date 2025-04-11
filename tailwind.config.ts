b2ec3f3bb2dc93d5309a657ad5416b9131f07ecd
Juel Rana <juels@Juels-Mac-mini.local>
2025-04-11 18:06:04 +0600

    Initial commit with REUI template

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
+export default config 
